109bc414a076e47ca77849d67f26036e92e2804e
Jean Gustavo Mohr <gustavomohr2002dj@gmail.com>
2023-09-26 17:39:58 -0300

    correção formulario.model

--- backend/src/models/formulario.model.ts
+++ backend/src/models/formulario.model.ts
@@ -1,18 +1,35 @@
-import { Entity } from 'typeorm'
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm'
 
 @Entity()
 export class FormularioModel {
-
+    @PrimaryGeneratedColumn()
     id_tipo_ticket: number;
-    @column({length})
+
+    @Column({length: 120})
     nomesolicitante: string;
+
+    @Column({length: 255})
     emailsolicitante: string;
+
+    @Column('int')
     telefone: string;
+
+    @Column('int')
     setor: number;
+
+    @Column({length: 1500})
     descricao: string;
+
+    @Column({})
     datacriacao: Date;
+
+    @Column({})
     etapa:string;
+
+    @Column({})
     abertura:number;
+    
+    @Column({})
     titulo: string;
 
 }
