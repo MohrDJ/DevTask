3e0410f40e0ba6b351f4a23aa1b97209f5399244
Jean Gustavo Mohr <gustavomohr2002dj@gmail.com>
2023-10-02 11:03:30 -0300

    correção app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { FormularioModule } from './modules/formulario/formulario.module';
 import { dataBaseConfigOracle } from './shared/database.provider';
-import { ArquivoController } from './upload/upload.controller';
-import { ArquivoService } from './upload/upload.service';
+import { ArquivoController } from './modules/upload/upload.controller';
+import { ArquivoService } from './modules/upload/upload.service';
 
 @Module({
   imports: [
