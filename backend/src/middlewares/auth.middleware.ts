5c5c71a5438c78adb1916ca45c7744f48b1c6191
Jean Gustavo Mohr <gustavomohr2002dj@gmail.com>
2023-10-04 10:51:58 -0300

    correção auth

--- backend/src/middlewares/auth.middleware.ts
+++ backend/src/middlewares/auth.middleware.ts
@@ -4,8 +4,8 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class CorsMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000/portal'); // Substitua pelo domínio do seu frontend
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // Substitua pelo domínio do seu frontend
+    res.header('Access-Control-Allow-Methods', 'GET, POST');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
   }
