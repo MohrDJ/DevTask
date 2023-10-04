5993522bbe243cf70cd1a9ec8ab8f125681113ee
Jean Gustavo Mohr <gustavomohr2002dj@gmail.com>
2023-10-04 09:48:24 -0300

    correção main

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,10 +4,23 @@ import { AppModule } from './app.module';
 import * as multer from 'multer';
 
 dotenv.config();
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
   // Configuração do Multer para armazenar os arquivos no diretório "uploads"
-  app.use(multer({ dest: './uploads' }).single('image'));
+  const storage = multer.diskStorage({
+    destination: './uploads',
+    filename: (req, file, callback) => {
+      callback(null, file.originalname); // Use o nome original do arquivo
+    },
+  });
+
+  const upload = multer({ storage }).single('image');
+
+  app.use(upload);
+
   await app.listen(process.env.APP_PORT);
 }
+
 bootstrap();
